9bd3a3f1af7c082ff05cea90abe76c3e771d2a67
kirubarajm <kirubarajmathavan@gmail.com>
2019-03-19 10:46:38 +0530

    moveit : add move it user responce message changed.

--- model/moveit/moveitUserModel.js
+++ model/moveit/moveitUserModel.js
@@ -20,14 +20,21 @@ var Moveituser = function(moveituser){
 Moveituser.createUser = function createUser(newUser, result) {    
         sql.query("INSERT INTO MoveitUser set ?", newUser, function (err, res) {
                 
-                if(err) {
-                    console.log("error: ", err);
-                    result(err, null);
-                }
-                else{
-                    console.log(res.insertId);
-                    result(null, res.insertId);
-                }
+            if(err) {
+                console.log("error: ", err);
+                result(err, null);
+            }
+            else{
+               let sucobj=true;
+               let message = 'Moveit user created successfully';
+                let resobj = {  
+                success: sucobj,
+                message:message
+                }; 
+
+             result(null, resobj);
+          
+            }
             });           
 };
 
