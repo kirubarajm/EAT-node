8f79f713dbd83850aa1c5cc57c2280735bc1d677
kirubarajm <kirubarajmathavan@gmail.com>
2019-07-17 19:53:03 +0530

    Collection list

---
+++ controllers/common/CollectionController.js
@@ -0,0 +1,51 @@
+"use strict";
+
+var Collection = require("../../model/common/collectionModel");
+
+exports.list_all_collection = function(req, res) {
+    Collection.list_all_active_collection(req.params,function(err, result) {
+    console.log("controller");
+    if (err) res.send(err);
+    console.log("res", result);
+    res.send(result);
+  });
+};
+
+exports.createcollection = function(req, res) {
+  var rc =new Collection(req.body);
+  Collection.createcollection(rc, function(err, result) {
+    if (err) res.send(err);
+    res.json(result);
+  });
+};
+
+
+exports.enable_disable_collection = function(req, res) {
+    Collection.updateBycollectionId(cid,req.params.activestatus, function(err, result) {
+      if (err) res.send(err);
+      res.json(result);
+    });
+};
+
+exports.read_all_collection_by_userid = function(req, res) {
+    Collection.getarefundcollection_by_userid(req.body, function(err, result) {
+    if (err) res.send(err);
+    res.json(result);
+  });
+};
+
+exports.delete_a_collection = function(req, res) {
+    Collection.remove(req.params.cid, function(err, result) {
+    if (err) res.send(err);
+    res.json(result);
+  });
+};
+
+exports.get_all_collection_by_userid = function(req, res) {
+    Collection.get_collection_by_userid(req.body, function(err, result) {
+    if (err) res.send(err);
+    res.json(result);
+  });
+};
+
+
---
+++ model/common/collectionModel.js
@@ -0,0 +1,117 @@
+"user strict";
+
+var sql = require("../db.js");
+const util = require('util');
+
+const query = util.promisify(sql.query).bind(sql);
+
+var Collection = function(collection) {
+  this.name = collection.name;
+  this.active_status = collection.active_status;
+  this.query = collection.query;
+  this.category = collection.category;  
+  this.collection_image = collection.collection_image;
+  this.collection_title = collection.collection_title;
+  this.collection_description = collection.collection_description;
+}
+
+
+Collection.createCoupon = function createCoupon(req, result) {
+
+
+ 
+      sql.query("INSERT INTO Collection set ?", req, function(err, res) {
+          if (err) {
+            result(err, null);
+          } else {
+            let sucobj = true;
+            let message = "Collection created successfully";
+            let resobj = {
+              success: sucobj,
+              message: message
+            };
+            result(null, resobj);
+          }
+        });
+      
+};
+
+
+Collection.list_all_active_collection = function list_all_active_collection(req,result) {
+  sql.query("Select * from Collections where active_status=1", function(err, res) {
+    if (err) {
+      console.log("error: ", err);
+      result(null, err);
+    } else {
+
+       if (res.length !== 0 ) {
+       
+        let resobj = {
+          success: true,
+          status:true,
+          result: res
+        };
+        result(null, resobj);
+       } else {
+        
+        let resobj = {
+          success: true,
+          status:false,
+          message: "Sorry there no active collections"
+        };
+        result(null, resobj);
+       }
+     
+    }
+  });
+};
+
+Collection.updateByCouponId = function(cid,active_status) {
+
+  var useddate=new Date();
+  sql.query(
+    "UPDATE Collection SET active_status=? WHERE cid = ?",
+    [active_status, cid],
+    function(err, res) {
+      if (err) {
+        console.log("error: ", err);
+        result(null, err);
+      } else {
+        result(null, res);
+      }
+    }
+  );
+};
+
+
+Collection.remove = function(cid, result) {
+  sql.query("DELETE FROM Collection WHERE cid = ? and active_status=1", [cid], function(err, res) {
+    if (err) {
+      console.log("error: ", err);  
+      result(null, err);
+    } else {
+      result(null, res);
+    }
+  });
+};
+
+Collection.getAllcoupon_by_user = function getAllcoupon_by_user(userid,result) {
+    sql.query("Select * from Collection where active_status=?",[userid], function(err, res) {
+      if (err) {
+        console.log("error: ", err);
+        result(null, err);
+      } else {
+        let sucobj = "true";
+        let resobj = {
+          success: sucobj,
+          result: res
+        };
+        result(null, resobj);
+      }
+    });
+  };
+
+
+
+
+module.exports = Collection;
