060d55989c46f0fe6620ec7bd795e28be97eb0ba
kirubarajm <kirubarajmathavan@gmail.com>
2019-11-18 18:13:55 +0530

    Make an function based on the dunzo constant

--- model/webhooks/dunzoModel.js
+++ model/webhooks/dunzoModel.js
@@ -9,7 +9,41 @@ var Dunzo = function(dunzo){
 };
 
 Dunzo.testingapi = function testingapi(req, result) {
-    console.log(dunzoconst);
+    //console.log(dunzoconst);
+    switch(req.state){
+        case dunzoconst.created:
+            console.log("created");
+            break;
+        case dunzoconst.queued:
+            console.log("queued");
+            break;
+        case dunzoconst.runner_accepted:
+            console.log("runner_accepted");
+            break;
+        case dunzoconst.runner_cancelled:
+            console.log("runner_cancelled");
+            break;
+        case dunzoconst.reached_for_pickup:
+            console.log("reached_for_pickup");
+            break; 
+        case dunzoconst.pickup_complete:
+            console.log("pickup_complete");
+            break;
+        case dunzoconst.started_for_delivery:
+            console.log("started_for_delivery");
+            break;
+        case dunzoconst.reached_for_delivery:
+            console.log("reached_for_delivery");
+            break;
+        case dunzoconst.delivered:
+            console.log("delivered");
+            break;
+        case dunzoconst.cancelled:
+            console.log("cancelled");
+            break;         
+        default:
+            console.log("No State");
+    }
     result(null, req);
 };
 
