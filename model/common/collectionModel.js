e89e48c639127cb50c46ce82c098cedcb7a0b89d
kirubarajm <kirubarajmathavan@gmail.com>
2020-01-02 16:54:09 +0530

    File Revert for by mistake new collection code added by praveen

--- model/common/collectionModel.js
+++ model/common/collectionModel.js
@@ -367,7 +367,7 @@ Collection.get_all_collection_by_cid = async function get_all_collection_by_cid(
 
       //based on logic this conditions will change
       ///product
-        if (req.cid == 1 || req.cid == 2 || req.cid == 4 || req.cid == 6 || req.cid == 7 || req.cid == 8 || req.cid == 9 || req.cid == 10) {
+        if (req.cid == 1 || req.cid == 2 || req.cid == 4 || req.cid == 6 || req.cid == 7) {
           var productlist = res[0].query + productquery  + groupbyquery + " ORDER BY mk.unservicable = 0 desc";
         }else if(req.cid == 3 ) {    ///kitchen
           var productlist = res[0].query + productquery  + orderbyquery;
@@ -403,7 +403,7 @@ Collection.get_all_collection_by_cid = async function get_all_collection_by_cid(
               for (let i = 0; i < res1.length; i++) {
                 res1[i].heading    = res[0].heading;
                 res1[i].subheading = res[0].subheading;
-                if (req.cid == 1 || req.cid == 2 || req.cid == 4 || req.cid == 6 || req.cid == 7 || req.cid == 8 || req.cid == 9 || req.cid == 10) {
+                if (req.cid == 1 || req.cid == 2 || req.cid == 4 || req.cid == 6 || req.cid == 7) {
                   res1[i].productlist =JSON.parse(res1[i].productlist);
                   //remove duplicate values
                   var arr = res1[i].productlist;
@@ -468,7 +468,7 @@ Collection.get_all_collection_by_cid = async function get_all_collection_by_cid(
               }
             }
 
-              if (req.cid == 1||req.cid == 4 ||req.cid == 2 || req.cid == 6|| req.cid == 7 || req.cid == 8 || req.cid == 9 || req.cid == 10) {
+              if (req.cid == 1||req.cid == 4 ||req.cid == 2 || req.cid == 6|| req.cid == 7) {
                 res1.sort((a, b) => parseFloat(a.status) - parseFloat(b.status));
               }
               
@@ -551,7 +551,7 @@ Collection.get_all_collection_by_cid_v2 = async function get_all_collection_by_c
         
   
         //based on logic this conditions will change
-           if (req.cid == 1 || req.cid == 2 || req.cid == 4 || req.cid == 6 || req.cid == 7 || req.cid == 8 || req.cid == 9 || req.cid == 10) {
+           if (req.cid == 1 || req.cid == 2 || req.cid == 4 || req.cid == 6 || req.cid == 7) {
           var productlist = res[0].query + productquery  + groupbyquery + " ORDER BY mk.unservicable = 0 desc";
         }else if(req.cid == 3 ) {    ///kitchen
           var productlist = res[0].query + productquery  + orderbyquery;
@@ -581,7 +581,7 @@ Collection.get_all_collection_by_cid_v2 = async function get_all_collection_by_c
             }
             
             for (let i = 0; i < res1.length; i++) {  
-                  if (req.cid == 1 || req.cid == 2 || req.cid == 4 || req.cid == 6 || req.cid == 7 || req.cid == 8 || req.cid == 9 || req.cid == 10) {
+                  if (req.cid == 1 || req.cid == 2 || req.cid == 4 || req.cid == 6 || req.cid == 7) {
                     res1[i].productlist =JSON.parse(res1[i].productlist)
                     var arr = res1[i].productlist;
                
@@ -648,7 +648,7 @@ Collection.get_all_collection_by_cid_v2 = async function get_all_collection_by_c
                   
                 }
   
-                if (req.cid == 1||req.cid == 4 ||req.cid == 2 || req.cid == 6  || req.cid == 7 || req.cid == 8 || req.cid == 9 || req.cid == 10) {
+                if (req.cid == 1||req.cid == 4 ||req.cid == 2 || req.cid == 6  || req.cid == 7) {
                   res1.sort((a, b) => parseFloat(a.status) - parseFloat(b.status));
                 }
   
