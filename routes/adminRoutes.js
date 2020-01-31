508343a05d45bca96c0a6e83e7fe87c2b162a670
kirubarajm <kirubarajmathavan@gmail.com>
2020-01-31 15:22:48 +0530

    reports url added

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -289,10 +289,10 @@ app.route("/admin/reports/cancelled_report_follow_up").post(routesVersioning({"1
 app.route("/admin/reports/unclosed_orders").post(routesVersioning({"1.0.0":orders.unclosed_orders}));
 app.route("/admin/reports/customerexperience").post(routesVersioning({"1.0.0":orders.customerexperience}));
 
-app.route("/admin/report/eatusergrowth").post(routesVersioning({"1.0.0":eatuser.user_growth_order_report}));
-app.route("/admin/report/makeit_earnings").post(routesVersioning({"1.0.0":makeituser.makeit_earnings_report}));
-app.route("/admin/report/makeit_cancellation").post(routesVersioning({"1.0.0":makeituser.makeit_cancellation_report}));
-app.route("/admin/report/makeit_avg_preparation").post(routesVersioning({"1.0.0":makeituser.makeit_avg_preparation_report}));
-app.route("/admin/report/xfactor_order_count").post(routesVersioning({"1.0.0":orders.xfactor_order_count}));
+app.route("/admin/reports/eatusergrowth").post(routesVersioning({"1.0.0":eatuser.user_growth_order_report}));
+app.route("/admin/reports/makeit_earnings").post(routesVersioning({"1.0.0":makeituser.makeit_earnings_report}));
+app.route("/admin/reports/makeit_cancellation").post(routesVersioning({"1.0.0":makeituser.makeit_cancellation_report}));
+app.route("/admin/reports/makeit_avg_preparation").post(routesVersioning({"1.0.0":makeituser.makeit_avg_preparation_report}));
+app.route("/admin/reports/xfactor_order_count").post(routesVersioning({"1.0.0":orders.xfactor_order_count}));
 
 }
