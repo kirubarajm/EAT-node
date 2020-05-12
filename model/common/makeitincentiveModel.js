689a37af1da48b21341207cb0bc9ae036ed8fbc1
kirubarajm <kirubarajmathavan@gmail.com>
2020-05-12 18:19:37 +0530

    start makeit Referral incentive function

--- model/common/makeitincentiveModel.js
+++ model/common/makeitincentiveModel.js
@@ -5,9 +5,12 @@ var sql = require("../db.js");
 var Makeitincentive = function(makeitincentive) {
   this.eligibility       = makeitincentive.eligibility;
   this.makeit_id         = makeitincentive.makeit_id;
-  this.complete_succession_count  = makeitincentive.complete_succession_count;
+  this.complete_succession_count    = makeitincentive.complete_succession_count;
   this.cancel_count      = makeitincentive.cancel_count;
   this.incentive_amount  = makeitincentive.incentive_amount;
+  this.makeit_aftercancel_earnings  = makeitincentive.makeit_aftercancel_earnings;
+  this.makeit_earnings   = makeitincentive.makeit_earnings;
+  this.makeit_referral_earnings     = makeitincentive.makeit_referral_earnings;
   this.from_date         = makeitincentive.from_date;
   this.to_date           = makeitincentive.to_date;
 };
